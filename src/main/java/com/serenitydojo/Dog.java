20e3d0bdc9cd215c3f1f7a83342f4ac388009efa
evrim <evrimcoskun@gmail.com>
2022-08-20 16:58:20 +0200

    Create makeNoise and feed method

--- src/main/java/com/serenitydojo/Dog.java
+++ src/main/java/com/serenitydojo/Dog.java
@@ -4,11 +4,21 @@ public class Dog {
     private final String name;
     private final String favoriteToy;
     private final int age;
+    private boolean isFed;
 
-    public Dog(String name, String favoriteToy, int age) {
+    static String DOG_NOISE = "Woof";
+
+    public Dog(String name, String favoriteToy, int age, boolean isFed) {
         this.name = name;
         this.favoriteToy = favoriteToy;
         this.age = age;
+        this.isFed = isFed;
+    }
+
+    public String makeNoise() { return DOG_NOISE; }
+
+    public boolean feed() {
+        return isFed = true;
     }
 
     public String getName() {
--- src/test/java/com/serenitydojo/CreatingADogTest.java
+++ src/test/java/com/serenitydojo/CreatingADogTest.java
@@ -7,7 +7,7 @@ public class CreatingADogTest {
 
     @Test
     public void creating_a_dog() {
-        Dog fido = new Dog("Fido","Bone", 5);
+        Dog fido = new Dog("Fido","Bone", 5, false);
         Assert.assertEquals(fido.getName(), "Fido");
         Assert.assertEquals(fido.getFavoriteToy(), "Bone");
         Assert.assertEquals(fido.getAge(), 5);
